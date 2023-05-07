8b5ffb98999f594605666d4d6e2d7bc9f3e84453
Michael Shimeles <michaelwasihun96@gmail.com>
2023-05-07 13:21:51 -0400

    Cleaned up token info

--- src/components/Buying/Buying.tsx
+++ src/components/Buying/Buying.tsx
@@ -1,28 +1,26 @@
-import { useGasPrice } from '@/hooks/useGasPrice';
 import { useGetMintWalletInfo } from '@/hooks/useGetMintWalletInfo';
 import { realTx } from '@/utils/buy';
 import {
-    Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, HStack,
+    Button,
+    Divider,
+    Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, HStack,
     Heading, Input,
-    Select,
-    Text, useDisclosure,
-    VStack,
-    useToast,
     Stat,
     StatArrow,
-    StatHelpText,
     StatLabel,
     StatNumber,
-    Tag,
-    TagCloseButton,
-    TagLabel
+    Text,
+    VStack,
+    useDisclosure,
+    useToast
 } from '@chakra-ui/react';
-import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import { useForm } from "react-hook-form";
-// import TokenInfo from '../TokenInfo/TokenInfo';
-import { useState } from 'react';
 import { useGetTokenInfo } from '@/hooks/useGetTokenInfo';
 import Link from 'next/link';
+import { useState } from 'react';
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+
 
 interface BuyingProps {
 
@@ -77,6 +75,7 @@ const Buying: React.FC<BuyingProps> = ({ }) => {
         setToken(e.target.value)
     }
     const { data } = useGetTokenInfo(token)
+    console.log("Token", data)
 
     return (
         <VStack w="100%" pt="2rem">
@@ -98,7 +97,6 @@ const Buying: React.FC<BuyingProps> = ({ }) => {
                     </HStack>
                 </VStack>
             </form>
-            {/* <TokenInfo tokenAddress={token} /> */}
             <Drawer
                 isOpen={isOpen}
                 placement='left'
@@ -106,77 +104,64 @@ const Buying: React.FC<BuyingProps> = ({ }) => {
             >
                 <DrawerOverlay />
                 <DrawerContent bgColor="blackAlpha.900" borderRight="1px solid"
-                    borderColor="gray.700"
-                >
-                    <DrawerCloseButton />
-                    <DrawerHeader bgColor="blackAlpha.400" borderBottom="1px solid" borderColor="gray.700">{data?.pairs?.[0]?.baseToken?.name + " " + "(" + data?.pairs?.[0]?.baseToken?.symbol + ")"}</DrawerHeader>
-
+                    borderColor="gray.700">
+                    <DrawerHeader bgColor="blackAlpha.400" borderBottom="1px solid" borderColor="gray.700" >
+                        <HStack justify="space-between" align="center">
+                            <Text>{data?.pairs?.[0]?.baseToken?.name + " " + "(" + data?.pairs?.[0]?.baseToken?.symbol + ")"}</Text>
+                            <Link href={data?.pairs?.[0]?.url} target="_blank">
+                                <ExternalLinkIcon />
+                            </Link>
+                        </HStack>
+                    </DrawerHeader>
                     <DrawerBody>
                         <VStack gap="1rem" w="full" pt="1rem">
                             <VStack bgColor="gray.900" p="1rem" rounded="2xl" w="full" align="flex-start">
-                                <Text>{"📊 Liquidity (USD): $" + data?.pairs?.[0]?.liquidity?.usd.toLocaleString()}</Text>
+                                <Text fontSize="sm">{"🏷️ Price: " + "$" + data?.pairs?.[0]?.priceUsd.toLocaleString()}</Text>
+                                <Text fontSize="sm">{"👾 Price: " + "Ξ" + data?.pairs?.[0]?.priceNative.toLocaleString()}</Text>
+                                <Text fontSize="sm">{"📊 Liquidity: $" + data?.pairs?.[0]?.liquidity?.usd.toLocaleString()}</Text>
+                                <Text fontSize="sm">{"💰 FDV: $" + (data?.pairs?.[0]?.fdv.toLocaleString())}</Text>
+                                <Text fontSize="xs">{"Created At: " + new Date(data?.pairs?.[0]?.pairCreatedAt).toLocaleDateString() + " " + new Date(data?.pairs?.[0]?.pairCreatedAt).toLocaleTimeString()}</Text>
                             </VStack>
                             <Stat bgColor="gray.900" p="1rem" rounded="2xl" w="full">
-                                <StatLabel>Price Change</StatLabel>
-                                <StatNumber>5m</StatNumber>
+                                <StatLabel>Token Stats</StatLabel>
+                                <StatNumber fontSize="lg">5m</StatNumber>
                                 {data?.pairs?.[0]?.priceChange?.m5 > 0 ? <StatArrow type='increase' /> : <StatArrow type='decrease' />}
                                 {data?.pairs?.[0]?.priceChange?.m5 + "%"}
-                                <Text>{"Buys: " + data?.pairs?.[0]?.txns?.m5?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.m5?.sells}</Text>
-
+                                <Divider pt="1rem" mb="1rem" w="full" />
+                                <Text fontSize="sm">{"Buys: " + data?.pairs?.[0]?.txns?.m5?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.m5?.sells}</Text>
+                                <Text fontSize="sm">{"Volume: $" + data?.pairs?.[0]?.volume?.m5.toLocaleString()}</Text>
                             </Stat>
                             <Stat bgColor="gray.900" p="1rem" rounded="2xl" w="full">
-                                <StatLabel>Price Change</StatLabel>
-                                <StatNumber>1hr</StatNumber>
+                                <StatLabel>Token Stats</StatLabel>
+                                <StatNumber fontSize="lg">1hr</StatNumber>
                                 {data?.pairs?.[0]?.priceChange?.h1 > 0 ? <StatArrow type='increase' /> : <StatArrow type='decrease' />}
                                 {data?.pairs?.[0]?.priceChange?.h1 + "%"}
-                                <Text>{"Buys: " + data?.pairs?.[0]?.txns?.h1?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.h1?.sells}</Text>
+                                <Divider pt="1rem" mb="1rem" w="full" />
+                                <Text fontSize="sm">{"Buys: " + data?.pairs?.[0]?.txns?.h1?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.h1?.sells}</Text>
+                                <Text fontSize="sm">{"Volume: $" + data?.pairs?.[0]?.volume?.h1.toLocaleString()}</Text>
                             </Stat>
                             <Stat bgColor="gray.900" p="1rem" rounded="2xl" w="full">
-                                <StatLabel>Price Change</StatLabel>
-                                <StatNumber>6hr</StatNumber>
+                                <StatLabel>Token Stats</StatLabel>
+                                <StatNumber fontSize="lg">6hr</StatNumber>
                                 {data?.pairs?.[0]?.priceChange?.h6 > 0 ? <StatArrow type='increase' /> : <StatArrow type='decrease' />}
                                 {data?.pairs?.[0]?.priceChange?.h6 + "%"}
-                                <Text>{"Buys: " + data?.pairs?.[0]?.txns?.h6?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.h6?.sells}</Text>
+                                <Divider pt="1rem" mb="1rem" w="full" />
+                                <Text fontSize="sm">{"Buys: " + data?.pairs?.[0]?.txns?.h6?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.h6?.sells}</Text>
+                                <Text fontSize="sm">{"Volume: $" + data?.pairs?.[0]?.volume?.h6.toLocaleString()}</Text>
                             </Stat>
                             <Stat bgColor="gray.900" p="1rem" rounded="2xl" w="full">
-                                <StatLabel>Price Change</StatLabel>
-                                <StatNumber>24hr</StatNumber>
+                                <StatLabel>Token Stats</StatLabel>
+                                <StatNumber fontSize="lg">24hr</StatNumber>
                                 {data?.pairs?.[0]?.priceChange?.h24 > 0 ? <StatArrow type='increase' /> : <StatArrow type='decrease' />}
                                 {data?.pairs?.[0]?.priceChange?.h24 + "%"}
-                                <Text>{"Buys: " + data?.pairs?.[0]?.txns?.h24?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.h24?.sells}</Text>
+                                <Divider pt="1rem" mb="1rem" w="full" />
+                                <Text fontSize="sm">{"Buys: " + data?.pairs?.[0]?.txns?.h24?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.h24?.sells}</Text>
+                                <Text fontSize="sm">{"Volume: $" + data?.pairs?.[0]?.volume?.h24.toLocaleString()}</Text>
                             </Stat>
                         </VStack>
-
-                        {/* <Text>{"Created at: " + (new Date(data?.pairs?.[0]?.pairCreatedAt).toLocaleDateString()) + " " + new Date(data?.pairs?.[0]?.pairCreatedAt).toLocaleTimeString()}</Text>
-                        <Text>{"FDV: $" + (data?.pairs?.[0]?.fdv.toLocaleString())}</Text>
-                        <Text>{"Price (USD): " + "$" + data?.pairs?.[0]?.priceUsd.toLocaleString()}</Text>
-                        <Text>{"Price (ETH): " + data?.pairs?.[0]?.priceNative.toLocaleString()}</Text>
-                        <Text>{"Liquidity (USD): $" + data?.pairs?.[0]?.liquidity?.usd.toLocaleString()}</Text>
-                        <Text as="u">Volume</Text>
-                        <Text>{"5m: $" + data?.pairs?.[0]?.volume?.m5.toLocaleString()}</Text>
-                        <Text>{"1h: $" + data?.pairs?.[0]?.volume?.h1.toLocaleString()}</Text>
-                        <Text>{"6h: $" + data?.pairs?.[0]?.volume?.h6.toLocaleString()}</Text>
-                        <Text>{"24h: $" + data?.pairs?.[0]?.volume?.h24.toLocaleString()}</Text>
-                        <Text as="u">Price Change</Text>
-                        <Text>{"5m: " + data?.pairs?.[0]?.priceChange?.m5 + "%"}</Text>
-                        <Text>{"1h: " + data?.pairs?.[0]?.priceChange?.h1 + "%"}</Text>
-                        <Text>{"6h: " + data?.pairs?.[0]?.priceChange?.h6 + "%"}</Text>
-                        <Text>{"24h: " + data?.pairs?.[0]?.priceChange?.h24 + "%"}</Text>
-                        <Text as="u">Transactions</Text>
-                        <Text>5m</Text>
-                        <Text>{"Buys: " + data?.pairs?.[0]?.txns?.m5?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.m5?.sells}</Text>
-                        <Text>1hr</Text>
-                        <Text>{"Buys: " + data?.pairs?.[0]?.txns?.h1?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.h1?.sells}</Text>
-                        <Text>6hr</Text>
-                        <Text>{"Buys: " + data?.pairs?.[0]?.txns?.h6?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.h6?.sells}</Text>
-                        <Text>24hr</Text>
-                        <Text>{"Buys: " + data?.pairs?.[0]?.txns?.h24?.buys + " " + "Sells: " + data?.pairs?.[0]?.txns?.h24?.sells}</Text> */}
                     </DrawerBody>
 
                     <DrawerFooter>
-                        <Link href={data?.pairs?.[0]?.url}>
-                            <Button variant='outline' mr={3}>Chart</Button>
-                        </Link>
                         <Button variant='outline' mr={3} onClick={onClose}>
                             Close
                         </Button>
